c46c693360c745303e177d1114e17cb12426402e
LucaLogallo <lucalogallo1998@gmail.com>
2021-05-26 17:46:26 +0200

    inizioi

---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrap">
+    <div class="headerRight">
+      <img src="" alt="">
+    </div>
+
+    <div class="headerRight">
+    <input type="text"
+    class="form-control"
+    placeholder="cerca"
+    v-model="search"
+    >
+    <button
+    @click="$emit('Search', search)"
+    
+    class="btn btn-primary">Cerca</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name:'Header',
+  data(){
+    return{
+      search:""
+    }
+}
+}
+</script>
+
+<style lang="scss" scoped>
+  .wrap{
+    height: 100px;
+    background-color: red;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+    padding: 2%;
+    .headerRight{
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      input{
+        height: 25px;
+        margin: 10px;
+      }
+      button{
+        padding: 5px;
+      }
+    }
+  }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+    <Movie />
+
+  </div>
+</template>
+
+<script>
+/* importo axios */
+import axios from 'axios';
+
+import Movie from '@/components/Movie'
+
+
+export default {
+  name:'Main',
+  components:{
+    Movie
+  },
+  data(){
+    return{
+      movies:[],
+      /* richiesta api */
+      api_url:'https://api.themoviedb.org/3/search/',
+      query: 'query=',
+      search: '',
+      movie: 'movie?',
+      tv:''
+    }
+  },
+  created(){
+   axios.get(this.api_url+)
+  }
+}
+</script>
+
+<style>
+
+</style>
