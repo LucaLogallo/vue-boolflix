a82b96e64b49bc66f5df8ad95019b760f169b5d5
LucaLogallo <lucalogallo1998@gmail.com>
2021-05-31 18:18:26 +0200

    commenti d'aiuto sia per finire carosello che per inserire le stesse funzionalità nel componente film

--- src/components/Tv.vue
+++ src/components/Tv.vue
@@ -1,23 +1,35 @@
+/* sui bottoni è stato messo il controllo che li fa apparire o scomparire se la lista (in questo caso di serie tv) esiste oppure no */
+
 <template>
   <div class="contenitore">
+    <!-- bottone per il precedente  -->
     <button class="prev"
     v-if="lista.length != 0"
     @click="prevCard()">
     <i class="fas fa-arrow-left"></i>
     </button>
+    <!-- /bottone per il precedente  -->
   <div class="carosello d-inline-flex">
 
+    <!-- card della "serie tv" -->
     <Card 
     :card = "lista[indexCard]"
     />
+    <!-- /card della "serie tv" -->
+
     
   </div>
+
+    <!-- bottone per il successivo -->
     <button class="next"
     v-if="lista.length != 0"
     @click="nextCard">
     <i class="fas fa-arrow-right"></i>
     </button>
+    <!-- /bottone per il successivo  -->
 
+
+  <!-- pallini della lista di "serie tv" generati dinamicamente -->
   <div class="pallini"
     v-if="lista.length != 0"
   >
@@ -49,12 +61,15 @@ export default {
     }
   },
   methods:{
+    /* funzione che gestisce l'indice della card in modo tale da prendere quella precedente a quella mostrata ogni volta che si clicca il tasto prev viene richiamata */
     prevCard(){
       this.indexCard --;
       if(this.indexCard < 0) {
         this.indexCard = this.lista.length - 1;
       }
     },
+
+    /* funzione che gestisce l'indice della card in modo tale da prendere quella successiva a quella mostrata ogni volta che si clicca il tasto next viene richiamata */
     nextCard(){
       this.indexCard ++;
       if(this.indexCard === this.lista.length){
@@ -67,7 +82,7 @@ export default {
 
 <style lang="scss">
 @import '@/assets/scss/general.scss';
-
+/* regole per il contenitore delle carte */
 .carosello{
   width: 100%;
   display: flex;
@@ -75,9 +90,11 @@ export default {
   align-items: center;
 }
 
+/* regole per il contenitore generale */
 .contenitore{
   position: relative;
 }
+
 .prev,.next{
   color:#bbbbbb;
   position:absolute;
@@ -85,9 +102,12 @@ export default {
   background: none;
   border: none;
 }
+/* tasto precedente */
 .prev{
   left: 10px;
 }
+
+/* tasto successivo */
 .next{
   right: 10px;
 }
